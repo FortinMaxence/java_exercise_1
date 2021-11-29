50ca1f7acaca2575fde1262c269dbf14f40d374d
Maxence Fortin <fortin@et.esiea.fr>
2021-11-29 15:13:35 +0100

    Add Fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -12,6 +12,30 @@ public class Launcher {
             {
                 return;
             }
+            else if(entree.equals("fibo")){
+                int n = scanner.nextInt();
+                int x0=0, x1=1, resultat=0;
+
+                if(n == 0){
+                    System.out.println(0);
+                    return;
+                }
+
+                if(n == 1){
+                    System.out.println(1);
+                    return;
+                }
+
+                for(int i=2; i<n+1; ++i)
+                {
+                    resultat = x0 + x1;
+                    x0 = x1;
+                    x1 = resultat;
+                }
+
+                System.out.println(resultat);
+                return;
+            }
             else
             {
                 System.out.println("Unknown command");
